022c7970855fe58009ee8d8984e201a7e6d6b8cb
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-04 16:14:46 -0700

    (cleanup) Reorganize server close #55

---
+++ server/middleware/devserver.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const config = require('./../../webpack.config.js');
+const devMiddleware = require('webpack-dev-middleware');
+const hotMiddleware = require('webpack-hot-middleware');
+
+const compiler = webpack(config);
+
+const webpackMiddleware = (server) => {
+  server.use(devMiddleware(compiler, {
+    hot: true,
+    filename: 'bundle.js',
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+      progress: true
+    },
+    historyApiFallback: true
+  }));
+
+  server.use(hotMiddleware(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000
+  }));
+};
+
+module.exports = webpackMiddleware;
