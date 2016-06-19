4e7a9cd8c25e7ab6a6f6438945405cc6cac3eb5b
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-19 03:49:32 -0700

    (test) Setup testing for reducers

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,5 +1,9 @@
-import { SEARCH_EVENTS } from './action_types';
+import { SEARCH_EVENTS, INITIAL_STATE } from './action_types';
 
 export const searchEventsAction = (query, state) => {
   return { type: SEARCH_EVENTS, payload: { query } };
+};
+
+export const intialStateAction = (query, state) => {
+  return { type: INITIAL_STATE };
 };
