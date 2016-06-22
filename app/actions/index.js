ae313df04bab6b1a517d885e126891027126ec7a
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-22 15:39:29 -0700

    (feat) Genre Filter close #80

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,9 +1,14 @@
-import { SEARCH_EVENTS, INITIAL_STATE } from './action_types';
+import { INITIAL_STATE, SEARCH_EVENTS, FILTER_EVENTS } from './action_types';
+
+export const intialStateAction = (state) => {
+  return { type: INITIAL_STATE, payload: state };
+};
 
 export const searchEventsAction = (query, state) => {
   return { type: SEARCH_EVENTS, payload: { query, state } };
 };
 
-export const intialStateAction = (query, state) => {
-  return { type: INITIAL_STATE };
+export const filterEventsAction = (genre, state) => {
+  return { type: FILTER_EVENTS, payload: { genre, state } };
 };
+
