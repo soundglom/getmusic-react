ad14871edc708f7e0bff1427d08356d14f129a00
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-23 04:28:43 -0700

    (cleanup) Cleanup filter components

--- app/containers/dropdown.jsx
+++ app/containers/dropdown.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Accordion, Panel } from 'react-bootstrap';
-import GenreFilter from '../components/genre-filter';
+import GenreFilter from '../components/filters/genre-filter';
 import TimeFilter from '../components/filters/time-filter';
 
 class DropDown extends Component {
@@ -8,8 +8,8 @@ class DropDown extends Component {
     return (
       <div id='drop-down'>
         <Accordion>
-          <Panel header='Genres' eventKey='1'><GenreFilter /></Panel>
-          <Panel header='Times' eventKey='2'><TimeFilter /></Panel>
+          <Panel header='GENRES' eventKey='1'><GenreFilter /></Panel>
+          <Panel header='TIMES' eventKey='2'><TimeFilter /></Panel>
         </Accordion>
       </div>
     );
--- app/reducers/filter-reducer.js
+++ app/reducers/filter-reducer.js
@@ -3,19 +3,13 @@ import { FILTER_EVENTS } from '../actions/action-types';
 const filterReducer = (state = {}, action) => {
   switch (action.type) {
     case FILTER_EVENTS:
-      // console.log(action.payload);
-      // console.log('Searching!', action.payload)
       let newState = {...action.payload};
       let filteredEvents = [];
 
-      console.log(newState);
       if (newState.prop === 'time') {
         let newTime = newState.filter.toLowerCase();
         let newObj = newState.state.filter.timeFilters[newTime];
 
-        // console.log('New time: ', newTime);
-        // console.log('Time: ', newObj);
-        console.log('Time: ', newObj[newTime]);
         filteredEvents = newObj.events;
       } else {
         filteredEvents = newState.state.allEvents.filter((event, i, l) => {
