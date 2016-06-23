4047cd4d7ead0427df3b4dc9261c1f72f6bf4525
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-22 21:46:32 -0700

    (cleanup) Clean up server

--- server/apis/eventbrite.js
+++ server/apis/eventbrite.js
@@ -1,1 +1,18 @@
-module.exports = 'https://www.eventbriteapi.com/v3/events/search/?sort_by=date&venue.city=&venue.region=CA&categories=103&expand=venue&token=';
+const fetch = require('../middleware/api-fetch');
+
+// const EVENTBRITE_QUERY = {
+//   root: 'https://www.eventbriteapi.com/v3/events/search/',
+//   sort: '?sort_by=date',
+//   city: '&venue.city='
+//   state: '&venue.region=CA',
+//   options: '&categories=103'
+// };
+
+const EVENTBRITE_URL = 'https://www.eventbriteapi.com/v3/events/search/?sort_by=date&venue.city=&venue.region=CA&categories=103&expand=venue&token=';
+const EVENTBRITE_KEY = process.env.EVENTBRITE;
+const url = `${EVENTBRITE_URL}${EVENTBRITE_KEY}`;
+
+module.exports = { 
+  fetch: fetch(url),
+  BASE_URL: EVENTBRITE_URL
+};
