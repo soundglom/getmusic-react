316f734cea75dbe94b576fe9b182f378a354f131
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-24 17:33:33 -0700

    (clean) Unnecessary landing.jsx imports

--- app/containers/landing.jsx
+++ app/containers/landing.jsx
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
-import foundation from 'foundation-sites';
 import TopBar from '../components/topbar';
-import 'semantic-ui/dist/components/form';
-// import Foundation from 'foundation-sites';
 
 export default class Landing extends Component {
   constructor(props) {
     super(props);
     this.state = { isRevealed: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  onComponentDidMount() {
-    // const bar = $('#offCanvas').foundation();
-    // bar.foundation();
-  }
-  handleClick(event, trigger) {
-    $('#offCanvas').foundation();
-    // console.log('I was clicked!');
-    // console.log(bar.foundation());
-    // this.setState({ isRevealed: !this.state.isRevealed });
-    // console.dir($('#offCanvas').foundation('toggle', event, this.state.isRevealed));
   }
   render() {
     return (
@@ -48,7 +32,7 @@ export default class Landing extends Component {
           <div className='off-canvas-content' data-off-canvas-content>
             {/* -- Page content-- */}
             <TopBar />
-            <h1 onClick={this.handleClick} data-toggle='offCanvas'>Landing Page</h1>
+            <h1 data-toggle='offCanvas'>Landing Page</h1>
           </div>
         </div>
       </div>
