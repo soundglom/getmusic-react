fe7e7616571b879818d6c4e21058e78deb981f9f
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-25 17:47:03 -0700

    (fix) Sass image loading

--- webpack.config.js
+++ webpack.config.js
@@ -36,25 +36,25 @@ module.exports = {
       { test: /jquery\.js$/, loader: 'expose?$' },
       { test: /jquery\.js$/, loader: 'expose?jQuery' },
       { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
+      { test: /\.scss$/, loaders: ['style', 'css', 'resolve-url', 'sass?sourceMap'] },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url?limit=10000!img?progressive=true' },
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url?file-loader?limit=10000!img?progressive=true' },
+      // {
+      //   test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+      //   loader: 'url?limit=10000&mimetype=application/font-woff'
+      // },
+      // {
+      //   test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+      //   loader: 'url?limit=10000&mimetype=application/octet-stream'
+      // },
+      // {
+      //   test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+      //   loader: 'file'
+      // },
+      // {
+      //   test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+      //   loader: 'url?limit=10000&mimetype=image/svg+xml'
+      // }
 
 
     ],
