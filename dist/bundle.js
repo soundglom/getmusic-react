8cb25398d32a0f8fa77d0b21033ae027050b373c
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-27 15:07:42 -0700

    (cleanup)

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "fc75bf6adc5ec7496828"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "38e5a16967100fcc53a5"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
