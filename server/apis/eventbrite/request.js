49fdc7e600a0aafde1a5e9c5a1d2f4ded5ed7b08
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2016-06-29 04:15:35 -0700

    (refactor) Clean up the backend

---
+++ server/apis/eventbrite/request.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const axios = require('axios');
+const dataFormat = require('./data-format.js');
+const EVENTBRITE_URL = 'https://www.eventbriteapi.com/v3/events/search/?sort_by=date&venue.city=San+Francisco&venue.region=CA&categories=103&expand=venue&token=';
+const EVENTBRITE_KEY = process.env.EVENTBRITE;
+
+
+// const EVENTBRITE_QUERY = {
+//   root: 'https://www.eventbriteapi.com/v3/events/search/',
+//   sort: '?sort_by=date',
+//   city: '&venue.city='
+//   state: '&venue.region=CA',
+//   options: '&categories=103'
+// };
+
+const eventbrite = `${EVENTBRITE_URL}${EVENTBRITE_KEY}`;
+
+const ebFetch = (arr) => {
+  return (req, res, next) => {
+    axios.get(eventbrite)
+      .then((response) => {
+        console.log(response)
+        arr.length = 0;
+        dataFormat(response.data.events, arr);
+      });
+    next();
+  };
+};
+
+module.exports = ebFetch;
