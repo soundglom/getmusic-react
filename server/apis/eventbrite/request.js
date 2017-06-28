96d5dae114ffec7a9f34150622117e4fc515e8eb
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-06-28 01:44:41 -0700

    (fix) update eventbrite url

--- server/apis/eventbrite/request.js
+++ server/apis/eventbrite/request.js
@@ -1,24 +1,23 @@
 const env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
-  console.log('Dev environment')
-  require('dotenv').config();  
+  console.log('Dev environment');
+  require('dotenv').config();
 }
 const axios = require('axios');
 const dataFormat = require('./data-format.js');
 const EVENTBRITE_KEY = process.env.EVENTBRITE_KEY;
-const EVENTBRITE_URL = 'https://www.eventbriteapi.com/v3/events/search/?sort_by=date&venue.city=San+Francisco&venue.region=CA&categories=103&expand=venue&token=';
-
-
+const SEARCH_URL = '?sort_by=date&location.address=1451+7th+St%2C+Oakland%2C+CA+94607&location.within=50mi&categories=103&start_date.keyword=this_month&token=';
+const BASE_URL = 'https://www.eventbriteapi.com/v3/events/search/';
+const EVENTBRITE_URL = `${BASE_URL}${SEARCH_URL}`;
+const eventbrite = `${EVENTBRITE_URL}${EVENTBRITE_KEY}`;
 // const EVENTBRITE_QUERY = {
 //   root: 'https://www.eventbriteapi.com/v3/events/search/',
 //   sort: '?sort_by=date',
 //   city: '&venue.city='
 //   state: '&venue.region=CA',
 //   options: '&categories=103'
-// };
+// https://www.eventbriteapi.com/v3/events/search/
 
-const eventbrite = `${EVENTBRITE_URL}${EVENTBRITE_KEY}`;
-console.log(eventbrite);
 const ebFetch = (arr) => {
   return (req, res, next) => {
     axios.get(eventbrite)
